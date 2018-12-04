952289abc5fb1be303f2dc9779a0b317fff139af
jhonatan.leon <jhonatan.leon@farmatodo.com>
2018-12-03 22:15:10 -0500

    se crean vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ArtistaComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
